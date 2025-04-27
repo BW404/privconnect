28f10f5d1446db4e732b3d835ca1c83004b0842a
BW404 <taj.darsana@gmail.com>
2025-04-27 21:26:57 +0600

    Add notification icon click event and update activity section styles

--- activity.js
+++ activity.js
@@ -1,5 +1,16 @@
-const trigger = document.getElementById("container");
-trigger.addEventListener("click", (event) => {
-	trigger.classList.add("clicked");
-	trigger.classList.toggle("active");
+const trigger1 = document.getElementById("container");
+const trigger2 = document.getElementById("noti_icon");
+trigger2.addEventListener("click", (event) => {
+	trigger1.classList.add("clicked");
+	trigger1.style.display = "block";
+	trigger1.classList.toggle("active");
 });
+
+
+// const trigger1 = document.getElementById("container");
+// const trigger2 = document.getElementById("noti_icon");
+
+// trigger2.addEventListener("click", () => {
+// 	trigger1.classList.add("clicked");
+//     trigger1.classList.add("active");
+// });
