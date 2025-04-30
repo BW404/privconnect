4b0ea4e66fbc445e00e8aea194a2a94e35a2e29b
BW404 <taj.darsana@gmail.com>
2025-05-01 02:48:37 +0600

    Update: 2025-05-01 02:48:36

--- assects/php/functions.php
+++ assects/php/functions.php
@@ -3,4 +3,21 @@ require_once 'config.php';
 require_once 'functions.php';
 require_once 'actions.php';
 
-function vli
+function validateSignupForm($form_data) {
+    $errors = [];
+
+    // Validate first name
+    if (empty($form_data['first_name'])) {
+        $errors[] = "First name is required.";
+    } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['first_name'])) {
+        $errors[] = "Only letters and white space allowed in first name.";
+    }
+
+    // Validate last name
+    if (empty($form_data['last_name'])) {
+        $errors[] = "Last name is required.";
+    } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['last_name'])) {
+        $errors[] = "Only letters and white space allowed in last name.";
+    }
+
+    // Validate
