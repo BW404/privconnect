b38d6a3fb92fbbf9a432c781e8ba889b6c5d6c9d
BW404 <taj.darsana@gmail.com>
2025-05-01 03:38:38 +0600

    Update: 2025-05-01 03:38:37

--- assects/php/actions.php
+++ assects/php/actions.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'functions.php';
 require_once 'config.php';
+require_once 'functions.php';
 if(isset($_GET['signup'])) {
     $response=validateSignupForm($_POST);
 if($response['status']){
