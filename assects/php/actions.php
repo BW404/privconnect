fac3c83fec8b55603c17bc31fa854051cb801f10
BW404 <taj.darsana@gmail.com>
2025-05-01 04:17:49 +0600

    Update: 2025-05-01 04:17:48

--- assects/php/actions.php
+++ assects/php/actions.php
@@ -3,9 +3,9 @@ require_once 'functions.php';
 if(isset($_GET['signup'])) {
     $response=validateSignupForm($_POST);
     if($response['status'] == false) {
-    echo isEmailRegistered('test@gmail.com');
         $_SESSION['error'] = $response;
         header("Location: ../pages/login.php?signup");
         exit();
     }
+    echo isEmailRegistered('test@gmail.com');
 }
