2db1d69409dfddd52e9b1cfee3cdd441c0fbaa79
BW404 <taj.darsana@gmail.com>
2025-05-01 02:55:58 +0600

    Update: 2025-05-01 02:55:57

--- assects/php/functions.php
+++ assects/php/functions.php
@@ -6,46 +6,10 @@ require_once 'actions.php';
 function validateSignupForm($form_data) {
     $response = array();
 
-    // Validate first name
-    if (empty($form_data['first_name'])) {
-        $response['msg'] = "First name is required.";
-        $response['status'] = "flase";
-        $response['field'] = "first_name";
-    } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['first_name'])) {
-        $response['msg'] =  = "Only letters and white space allowed in first name.";
-        $response['status'] = "flase";
-        $response['field'] = "first_name";
-    }
 
-    // Validate last name
-    if (empty($form_data['last_name'])) {
-        $response['msg'] = "Last name is required.";
-        $response['status'] = "flase";
-        $response['field'] = "last_name";
-    } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['last_name'])) {
-        $response['msg'] = "Only letters and white space allowed in last name.";
-        $response['status'] = "flase";
-        $response['field'] = "last_name";
 
-    // Validate email
-    if (empty($form_data['email'])) {
-        $response['msg'] = "Email is required.";
-        $response['status'] = "flase";
-        $response['field'] = "email";
-    } elseif (!filter_var($form_data['email'], FILTER_VALIDATE_EMAIL)) {
-        $response['msg'] = "Invalid email format.";
-        $response['status'] = "flase";
-        $response['field'] = "email";
-    }
-    // Validate username
-    if (empty($form_data['username'])) {
-        $response['msg'] = "Username is required.";
-        $response['status'] = "flase";
-        $response['field'] = "username";
-    } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $form_data['username'])) {
-        $response['msg'] = "Only letters and numbers allowed in username.";
-        $response['status'] = "flase";
-        $response['field'] = "username";
+
+
     }
     // Validate password
 
@@ -58,3 +22,49 @@ function validateSignupForm($form_data) {
         $response['status'] = "flase";
         $response['field'] = "password";
     }
+
+        // Validate username
+        if (empty($form_data['username'])) {
+            $response['msg'] = "Username is required.";
+            $response['status'] = "flase";
+            $response['field'] = "username";
+        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $form_data['username'])) {
+            $response['msg'] = "Only letters and numbers allowed in username.";
+            $response['status'] = "flase";
+            $response['field'] = "username";
+        }
+
+        // Validate first name
+        if (empty($form_data['first_name'])) {
+            $response['msg'] = "First name is required.";
+            $response['status'] = "flase";
+            $response['field'] = "first_name";
+        } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['first_name'])) {
+            $response['msg'] =  = "Only letters and white space allowed in first name.";
+            $response['status'] = "flase";
+            $response['field'] = "first_name";
+        }
+
+            // Validate last name
+    if (empty($form_data['last_name'])) {
+        $response['msg'] = "Last name is required.";
+        $response['status'] = "flase";
+        $response['field'] = "last_name";
+    } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['last_name'])) {
+        $response['msg'] = "Only letters and white space allowed in last name.";
+        $response['status'] = "flase";
+        $response['field'] = "last_name";
+    }
+    
+
+    // Validate email
+    if (empty($form_data['email'])) {
+        $response['msg'] = "Email is required.";
+        $response['status'] = "flase";
+        $response['field'] = "email";
+    } elseif (!filter_var($form_data['email'], FILTER_VALIDATE_EMAIL)) {
+        $response['msg'] = "Invalid email format.";
+        $response['status'] = "flase";
+        $response['field'] = "email";
+    }
+
