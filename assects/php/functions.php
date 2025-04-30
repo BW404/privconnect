15e77efb458d65838e06f61fd611c6ce56533e56
BW404 <taj.darsana@gmail.com>
2025-05-01 02:50:24 +0600

    Update: 2025-05-01 02:50:24

--- assects/php/functions.php
+++ assects/php/functions.php
@@ -4,11 +4,12 @@ require_once 'functions.php';
 require_once 'actions.php';
 
 function validateSignupForm($form_data) {
-    $errors = [];
+    $response = array();
 
     // Validate first name
     if (empty($form_data['first_name'])) {
-        $errors[] = "First name is required.";
+        $response['msg'] = "First name is required.";
+        $response['status'] = "flase";
     } elseif (!preg_match("/^[a-zA-Z ]*$/", $form_data['first_name'])) {
         $errors[] = "Only letters and white space allowed in first name.";
     }
