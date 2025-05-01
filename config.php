05cab418b3b79064a25dad6693257f9c8fc52625
BW404 <taj.darsana@gmail.com>
2025-05-01 10:06:41 +0600

    Update: 2025-05-01 10:06:41

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'privconnect_db');
+define('DB_NAME', 'privconnect_db_1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
