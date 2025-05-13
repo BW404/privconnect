1713a606b0d6acfe681c842dd6c2578a30e25119
BW404 <taj.darsana@gmail.com>
2025-05-14 01:42:49 +0600

    Updated files:

--- assects/php/functions.php
+++ assects/php/functions.php
@@ -106,7 +106,7 @@ function isUsernameRegistered($username) {
 function createUser($form_data) {
     global $conn;
     $hashed_password = password_hash($form_data['password'], PASSWORD_BCRYPT);
-    $query = "INSERT INTO users (first_name,last_name,gender,email,username,password) VALUES ('".$form_data['first_name']."', '".$form_data['last_name']."', '".$form_data['gender']."', '".$form_data['email']."', '".$form_data['username']."', '".$form_data['password']."')";
+    $query = "INSERT INTO users (first_name,last_name,gender,email,username,password) VALUES ('".$form_data['first_name']."', '".$form_data['last_name']."', '".$form_data['gender']."', '".$form_data['email']."', '".$form_data['username']."', '".$hashed_password."')";
     $run = mysqli_query($conn, $query);
     return $run;
 }
