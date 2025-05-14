89ed01c3a9308ed37bb69713328eb5fba4b14475
BW404 <taj.darsana@gmail.com>
2025-05-15 03:50:48 +0600

    2025-05-15 03:50:48

--- assects/php/functions.php
+++ assects/php/functions.php
@@ -361,7 +361,7 @@ function createPost($text, $image) {
 // for getting post
 function getPosts() {
     global $conn;
-    $query = "SELECT posts.id, posts.post_text, posts.post_img, users.first_name, users.last_name, users.profile_pic FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC";
+    $query = "SELECT posts.id, posts.user_id,posts.post_text, posts.post_img, users.first_name, users.last_name, users.profile_pic FROM posts JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC";
     $run = mysqli_query($conn, $query);
     return mysqli_fetch_all($run,true);
 }
