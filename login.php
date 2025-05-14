b87559177cef021c0a9dc8f16ec33cb0cc0a9de3
BW404 <taj.darsana@gmail.com>
2025-05-14 20:27:03 +0600

    2025-05-14 20:27:03

--- login.php
+++ login.php
@@ -82,7 +82,7 @@ if (isset($conn)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PrivConnect Login / Signup</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/material-icons.min.css">
 </head>
