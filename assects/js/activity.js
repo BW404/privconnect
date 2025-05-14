230b6562e793353c4750968f2572c1dbc9526eb2
BW404 <taj.darsana@gmail.com>
2025-05-14 20:37:11 +0600

    2025-05-14 20:37:11

--- assects/js/activity.js
+++ assects/js/activity.js
@@ -5,3 +5,19 @@ notificationIcon.addEventListener("click", () => {
     // Toggle the 'active' class to show/hide the notification panel with animation
     notificationPanel.classList.toggle("active");
 });
+
+
+
+const userIcon = document.getElementById("user_icon");
+const dropdownMenu = document.getElementById("dropdown_menu");
+
+userIcon.addEventListener("click", () => {
+    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+});
+
+// Close the dropdown when clicking outside
+document.addEventListener("click", (event) => {
+    if (!userIcon.contains(event.target)) {
+        dropdownMenu.style.display = "none";
+    }
+});
