6f66fd433fb730d8ab5aa0cc623ffafc7b99c229
BW404 <taj.darsana@gmail.com>
2025-05-15 03:45:43 +0600

    2025-05-15 03:45:42

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'assects/php/functions.php';
+
+
+
+
+
+
+
 if(isset($_SESSION['Auth']) && $_SESSION['Auth'] == true) {
     // User is already logged in, redirect to dashboard
     header('Location: assects\pages\dashboard.php');
