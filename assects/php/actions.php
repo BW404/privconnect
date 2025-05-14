3ed42e1bf9061267c548939f87c61fd4af70fc94
BW404 <taj.darsana@gmail.com>
2025-05-14 19:36:35 +0600

    2025-05-14 19:36:35

--- assects/php/actions.php
+++ assects/php/actions.php
@@ -40,6 +40,7 @@ if(isset($_GET['login'])) {
         else{
             $_SESSION['error'] = "Error logging in.";
             $_SESSION['error'] = "Invalid username or password.";
+            $_SESSION[]
             header("Location: ../pages/login.php?login");
             exit();
         }
