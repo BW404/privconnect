27e93e2e321f37f634989dd1940cbb28f2a962ea
BW404 <taj.darsana@gmail.com>
2025-05-15 03:53:21 +0600

    2025-05-15 03:53:21

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 require_once 'assects/php/functions.php';
 
 echo "<pre>";
-print_r(getPosts());
 
 
 
 
 
-// if(isset($_SESSION['Auth']) && $_SESSION['Auth'] == true) {
-//     // User is already logged in, redirect to dashboard
-//     header('Location: assects\pages\dashboard.php');
-//     exit;
-// }
-// // Redirect to login page
-// header('Location: assects\pages\login.php');
-// exit;
+
+if(isset($_SESSION['Auth']) && $_SESSION['Auth'] == true) {
+    // User is already logged in, redirect to dashboard
+    header('Location: assects\pages\dashboard.php');
+    exit;
+}
+// Redirect to login page
+header('Location: assects\pages\login.php');
+exit;
 // ?>
