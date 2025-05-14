7942b9b8a5523b1f768a59c1a3d3a43d11eef589
BW404 <taj.darsana@gmail.com>
2025-05-14 20:20:38 +0600

    2025-05-14 20:20:38

--- assects/pages/dashboard.php
+++ assects/pages/dashboard.php
@@ -1,24 +1,8 @@
 <?php
 include_once '../php/functions.php';
-include '../php/header.php';
+include '../php/header.php';?>
 <body>
-    <nav>
-        
-        <div class="nav-left">
-            <h2 class="logo"><a href="#">Priv<span>Connect</span></a></h2>
-            <ul>
-                <li id="noti_icon" class="icons-nav"><img src="../icons/notification.png" alt="notification"></li>
-                <li class="icons-nav"><img src="../icons/inbox.png" alt="inbox"></li>
-            </ul>
-        </div>
-        <div class="nav-right">
-            <div class="search-box">
-                <img src="../icons/search.png" alt="search">
-                <input type="text" placeholder="Search">
-            </div>
-            <div class="nav-user-icon online">
-                <img src="../icons/profile-pic.png" alt="profile-pic">
-    </nav>
+
 
     <div class="container">
         <div class="left-sidebar">
