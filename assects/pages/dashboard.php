5caf522428ceee8fe43bb040872d43f1be116a89
BW404 <taj.darsana@gmail.com>
2025-05-15 05:33:32 +0600

    2025-05-15 05:33:32

--- assects/pages/dashboard.php
+++ assects/pages/dashboard.php
@@ -112,39 +112,60 @@ include '../php/header.php';?>
             ?>
 
         </div>
+                    echo '<img src="../photos/posts/'.$post['post_img'].'" alt="" class="post-img">';
+                }
+                
+                echo '<div class="post-row">';
+                echo '<div class="activity-icons">';
+                // echo '<div><img src="../icons/like.png" alt="">'.$post['likes'].'</div>';
+                // echo '<div><img src="../icons/comments.png" alt="">'.$post['comments'].'</div>';
+                // echo '<div><img src="../icons/share.png" alt="">'.$post['shares'].'</div>';
+
+                echo '</div>';
+                echo '<div class="post-profile-icon">';
+                echo '<img src="./Socialbook_img/profile-pic.png" alt=""><i class="fas fa-caret-down"></i>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+
+          
+<!--             
+            <button type="button" class="load-more-btn">Load More</button> -->
+        </div>
        
         <div class="right-sidebar">
+
 
              <div class="sidebar-title">
                 <h4>
                     Conversation
                 </h4>
-                <a href="#" id="hide-chat-link">
+                <a href="#">
                     Hide Chat
                 </a>
             </div>
-
-            <div id="user-list" class="user-list">
-                <!-- User list will be populated here by JS -->
+            <div class="online-list">
+                <div class="online">
+                    <img src="../icons/member-2.png" alt="">
+                </div>
+                <p>Roy Clark</p>
             </div>
-
-            <div id="chat-container" class="chat-container" style="display:none;">
-                <div class="chat-header">
-                    <h5 id="chat-user-name">Select a user to chat</h5>
+            <div class="online-list">
+                <div class="online">
+                    <img src="../icons/member-3.png" alt="">
                 </div>
-                <div id="chat-messages" class="chat-messages">
-                    <!-- Chat messages will be populated here -->
-                </div>
-                <div class="chat-input">
-                    <textarea id="chat-message-input" placeholder="Type your message..."></textarea>
-                    <button id="send-message-btn">Send</button>
-                </div>
+                <p>Sieena Watson</p>
             </div>
-
+            <div class="online-list">
+                <div class="online">
+                    <img src="../icons/member-4.png" alt="">
+                </div>
+                <p>Ben Taylor</p>
+            </div>
         </div>
     </div>
     <script src="../js/activity.js"></script>
 
     <?php include '../php/footer.php';?>
-</body>
-</html>
