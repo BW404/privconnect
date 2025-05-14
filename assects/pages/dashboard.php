55a524448a10e78834ee2b41dcffef87c38e98b2
BW404 <taj.darsana@gmail.com>
2025-05-15 04:05:51 +0600

    2025-05-15 04:05:51

--- assects/pages/dashboard.php
+++ assects/pages/dashboard.php
@@ -93,9 +93,9 @@ include '../php/header.php';?>
             <?php 
             // Fetch posts from the database
             $posts=getPosts();
-            echo "<pre>";
-print_r(getPosts());
-echo "</pre>";
+//             echo "<pre>";
+// print_r(getPosts());
+// echo "</pre>";
             foreach($posts as $post) {
                 // Display each post
                 echo '<div class="post-container">';
