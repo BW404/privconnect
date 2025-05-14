4470c990f40c14cfbb81dc6b67b5c8ab83a8049c
BW404 <taj.darsana@gmail.com>
2025-05-14 20:20:05 +0600

    2025-05-14 20:20:05

--- assects/pages/dashboard.php
+++ assects/pages/dashboard.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PrivConnect - Dashboard</title>
-    <link rel="stylesheet" href="../css/dashboard.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/material-icons.min.css">
-</head>
+<?php
+include_once '../php/functions.php';
+include '../php/header.php';
 <body>
     <nav>
         
